e0bff02373dc00e709654bcb0a866dd427fb5643
JustasMit <44390650+JustasMit@users.noreply.github.com>
2022-01-24 09:43:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ import Collapse from "@mui/material/Collapse"
 import Box from "@mui/material/Box"
 
 import "./css/app.css"
-//todo - memory router (popup pagination), error when backward after refresh (redirect to / ?), route errors after refresh, unmount popup components
-//reset search/table
+
 const App = () => {
 	const [selectedObjectFilter, setSelectedObjectFilter] = useState("")
 	const [selectedMemoryFilter, setSelectedMemoryFilter] = useState("")
